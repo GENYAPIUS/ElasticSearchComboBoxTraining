6b99c15182435919c96a82edb965e1142023bd83
GENYA <t6589989@gmail.com>
2022-09-23 09:38:58 +0800

    feat: Add main view model

---
+++ ElasticSearchComboBoxTraining/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using WPFLibrary;
+
+namespace ElasticSearchComboBoxTraining;
+
+public class MainViewModel : NotifyPropertyBase
+{
+    public MainViewModel()
+    {
+        Options = GetOptions();
+    }
+
+    private ObservableCollection<string> _options = new();
+
+    public ObservableCollection<string> Options
+    {
+        get => _options;
+        set => SetProperty(ref _options, value);
+    }
+
+    private ObservableCollection<string> GetOptions()
+    {
+        return new ObservableCollection<string>()
+        {
+            "Option1",
+            "Option2",
+            "Option3"
+        };
+    }
+}
